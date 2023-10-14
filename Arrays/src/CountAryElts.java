5c97a016c7743b571cd7d678782b07d748c4e326
Jagrit <rjagrit.2k@gmail.com>
2023-10-15 01:00:31 +0530

    Initial commit

---
+++ Arrays/src/CountAryElts.java
@@ -0,0 +1,11 @@
+
+//Q. Find Number of Elements in an Array in Java
+public class CountAryElts
+{
+    public static void main(String [] args)
+    {
+        int ar[]={1,2,3,4,5,6,7,8,9,10,2,4,5,6,7};
+        int length=ar.length;
+        System.out.println("There are "+length+" elements in given array");
+    }
+}
